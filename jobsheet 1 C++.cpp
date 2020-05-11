5c3116da69df5797d6d1895543e7125f99dd550c
RiyanFR19125 <64636825+RiyanFR19125@users.noreply.github.com>
2020-05-11 21:22:03 +0800

    Create jobsheet 1 C++.cpp
    membuat array satu dimensi

---
+++ jobsheet 1 C++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+main (){
+char* my_array[9]={"f1b019123","f1b019124","f1b019125","f1b019127","f1b019146",};
+	
+	cout<<"nim mahasiswa ke- 1 = "<<my_array[0]<<"\n"; //rio sarifudin
+	cout<<"nim mahasiswa ke- 2 = "<<my_array[1]<<"\n"; //risma prasasti
+	cout<<"nim mahasiswa ke- 3 = "<<my_array[2]<<"\n"; //riyan fathurrahman
+	cout<<"nim mahasiswa ke- 4 = "<<my_array[3]<<"\n"; //rohul iman
+	cout<<"nim mahasiswa ke- 5 = "<<my_array[4]<<"\n"; //zahratul laily
+
+}
